dd9bcbb3a3029d560cf582840b645014ff6cde1b
bhavinp20 <bhavinpatel420@gmail.com>
2020-09-19 18:03:49 -0400

    first commit

---
+++ src/tools.h
@@ -0,0 +1,37 @@
+#ifndef TOOLS_H_
+#define TOOLS_H_
+
+#include <vector>
+#include "Eigen/Dense"
+
+class Tools {
+ public:
+  /**
+   * Constructor.
+   */
+  Tools();
+
+  /**
+   * Destructor.
+   */
+  virtual ~Tools();
+
+  /**
+   * A helper method to calculate RMSE.
+   */
+  Eigen::VectorXd CalculateRMSE(const std::vector<Eigen::VectorXd> &estimations, 
+                                const std::vector<Eigen::VectorXd> &ground_truth);
+
+  /**
+   * A helper method to calculate Jacobians.
+   */
+  Eigen::MatrixXd CalculateJacobian(const Eigen::VectorXd& x_state);
+  
+  
+   /**
+   * A helper method to calculate Covariant Matrix.
+   */
+  Eigen::MatrixXd CalculateCovariantMatrix(const double dt, const double noise_ax, const double noise_ay);
+};
+
+#endif  // TOOLS_H_
